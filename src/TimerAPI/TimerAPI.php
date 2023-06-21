254c90e1234d0c1e1819086b07e3c540e21e09c8
AmitxD <81667979+Amitminer@users.noreply.github.com>
2023-06-21 12:19:51 +0530

    update TimerAPI.php

--- src/TimerAPI/TimerAPI.php
+++ src/TimerAPI/TimerAPI.php
@@ -17,9 +17,10 @@ class TimerAPI extends PluginBase {
     }
 
     public function onEnable(): void {
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->getLogger()->info("§aSuccessfully enabled TimerAPI!");
     }
+
+    // TODO: Add task scheduling methods and task cancellation
 
     /**
      * Waits for a specified duration and then executes a callback function.
@@ -30,11 +31,19 @@ class TimerAPI extends PluginBase {
     public static function wait(callable $callback, int $duration): void {
         TimerAPI::getInstance()->getScheduler()->scheduleDelayedTask(new ClosureTask($callback), $duration * 20);
     }
-    // TODO:
-    # scheduleTask 
-    # scheduleRepeatingTask
-    # scheduleDelayedRepeatingTask 
-    # Cancel Task!
+
+    /**
+     * Schedules a task to be repeated at a specified interval for a specific number of repetitions.
+     *
+     * @param callable $callback     The callback function to execute.
+     * @param int      $interval     The interval between each repetition in seconds.
+     * @param int      $repetitions  The number of times the task should be repeated.
+     */
+    public static function repeat(callable $callback, int $interval, int $repetitions): void {
+        $task = new ClosureTask($callback);
+        $scheduler = TimerAPI::getInstance()->getScheduler();
+        $scheduler->scheduleRepeatingTask($task, $interval * 20, $repetitions);
+    }
 
     /**
      * Gets the instance of the TimerAPI plugin.
